ed5c3678236d4ff4f5642b2d3384af7a24ee1572
johnelliott <johnelliott703@gmail.com>
2014-12-13 17:08:23 -0500

    add jslint helpers to most js files

--- controllers/runs.js
+++ controllers/runs.js
@@ -1,27 +1,26 @@
-(function() {
-    'use strict';
-    var model = require('../models/runs');
-    var _ = require('underscore');
-    
-    // save runs to model
-    exports.store = function(req, res, next) {
-	// save request body in case we need it
-	var reqBody = _.clone(req.body);
-	var runs = [{"message":"hello from the run"}]; // arduino poll stub
-	model.store(runs, function(err, data){
-	    if (err) return res.send(503, err);
-	    next();
-	});
-    };
-    
- // remove older runs from model
-    exports.trim = function(req, res, next) {
+/*jslint node: true */
+'use strict';
+
+var model = require('../models/runs');
+var _ = require('underscore');
+
+// save runs to model
+exports.store = function(req, res, next) {
+    // save request body in case we need it
+    var reqBody = _.clone(req.body);
+    var runs = [{"message":"hello from the run"}]; // arduino poll stub
+    model.store(runs, function(err, data){
+	if (err) return res.send(503, err);
 	next();
-    };
+    });
+};
 
- // send runs to server
-    exports.send = function(req, res, next) {
-	res.send('success');
-    };
+// remove older runs from model
+exports.trim = function(req, res, next) {
+    next();
+};
 
-})();
+// send runs to server
+exports.send = function(req, res, next) {
+    res.send('success');
+};
--- models/runs.js
+++ models/runs.js
@@ -1,19 +1,19 @@
-(function(){
-    'use strict';
+/*jslint node: true */
+'use strict';
 
-    var redis = require('../lib/redis');
-    var counter = require('../lib/counter');
-    
-    // Save runs to database (redis)
-    // params are runs, an array
-    // and callback, a function
-    exports.store = function(runs, callback) {
- 	if (!runs.length) return callback(null, null);
-	var run = runs.pop();
-	console.log(run);
-	redis.lpush('runs', JSON.stringify(run), function(err, data) {
-	    if (err) return callback(err, null);
-	    exports.store(err, callback);
-	});
-    };
-})();
+var redis = require('../lib/redis');
+var counter = require('../lib/counter');
+
+// Save runs to database (redis)
+// params are runs, an array
+// and callback, a function
+exports.store = function(runs, callback) {
+    if (!runs.length) return callback(null, null);
+    var run = runs.pop();
+    console.log(run);
+    redis.lpush('runs', JSON.stringify(run), function(err, data) {
+	if (err) return callback(err, null);
+	exports.store(err, callback);
+    });
+};
+
