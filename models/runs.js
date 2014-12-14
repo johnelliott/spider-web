55b12c87cfa66877466fa0a6e7096699a5785483
johnelliott <johnelliott703@gmail.com>
2014-12-14 16:43:54 -0500

    work with logs. Now I am deciding to abandon routes for getting data, since it comes in locally

--- models/runs.js
+++ models/runs.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var redis = require('../lib/redis');
-var counter = require('../lib/counter');
 
 // Save runs to database (redis)
 // params are runs, an array
@@ -10,7 +9,8 @@ var counter = require('../lib/counter');
 exports.store = function(runs, callback) {
     if (!runs.length) return callback(null, null);
     var run = runs.pop();
-    redis.lpush('runs', JSON.stringify(run), function(err, data) {
+
+    redis.lpush('runs', run, function(err, data) {
 	if (err) return callback(err, null);
 	exports.store(err, callback);
     });
