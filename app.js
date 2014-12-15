ab5c6e4f93901d691dd4b1e7aeaf8a92664b134e
johnelliott <johnelliott703@gmail.com>
2014-12-14 20:47:33 -0500

    Combine all into app.js for ease of development

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
 /*jslint node: true */
 'use strict';
 
+// App
 var config = require('./config');
 var express = require('express');
 var bodyParser = require('body-parser');
 var runs = require ('./controllers/runs');
 var app = express();
 var port = process.env.PORT || 8000;
+var _ = require('underscore');
 
-// Listen on port 8000
-app.listen(port, function(){
-    console.log("listening on port %d", port);
+// Serial port
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
+var localSerialPort = process.env.LOCALSERIALPORT || '/dev/ttyACM0'; // default to Ras. Pi USB
+
+// Instantiate a serial port
+var sp = new SerialPort(localSerialPort, {
+    parser: serialport.parsers.readline("\n"),
+    baudrate: 9600
+});
+
+sp.open(function (error) {
+    if ( error ) {
+    console.log('Failed to open serial port: ' + error);
+    } else {
+    console.log('Serial port open: ' + sp.path);
+    }
+});
+
+sp.on("data", function (data) {
+    console.log(data);
 });
 
 // expect to receive json and parse if it checks out
 app.use(bodyParser.json());
 
 // routes
-app.get('/', runs.get, function(req, res){
+app.get('/', runs.send, function(req, res){
     res.send('\ndone\n');
+});
+
+
+// Listen on port 8000
+app.listen(port, function(){
+    console.log("listening on port %d", port);
 });
