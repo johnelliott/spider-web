3dd24be6a7d63f5b248df917c7224e0d815aba94
johnelliott <johnelliott703@gmail.com>
2015-01-16 11:39:11 -0500

    remove extra semicolon

--- config.js
+++ config.js
@@ -4,6 +4,5 @@ var os = require('os');
 
 process.env.PORT = 8000;
 process.env.SOCKPORT = 3000;
-;
 process.env.LOCALSERIALPORT = os.platform()=="darwin" ? "/dev/cu.usbmodem1421" : "/dev/ttyACM0"; // default to Ras. Pi USB
 
