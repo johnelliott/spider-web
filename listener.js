4e106140e1ab430f39865c44ee4780709fde6483
John Elliott <johnelliott703@gmail.com>
2015-04-25 14:21:30 -0400

    reorganize demo apps

--- listener.js
+++ listener.js
@@ -2,7 +2,7 @@ var faye = require('faye');
 var config = require('./config');
 
 var client = new faye.Client('http://localhost:' +
-    process.env.LISTENERPORT || 3001 +
+    process.env.LISTENERPORT +
     '/hits');
 console.log('Faye listening on port ' + process.env.LISTENERPORT);
 var subscription = client.subscribe('/hits', function(message) {
--- talker.js
+++ talker.js
@@ -1,21 +1,15 @@
 /*jslint node: true */
-
-// App
+var http = require('http');
+var faye = require('faye');
 var config = require('./config');
 var sockPort = process.env.SOCKPORT || 8001;
 
-// Sockets with Faye
-var http = require('http');
-var faye = require('faye');
-
 var bayeux = new faye.NodeAdapter({mount: '/hits', timeout: 45});
-
 // Handle non-Bayeux requests
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end("Hello, non-Bayeux request\n");
 });
-
 bayeux.attach(server);
 server.listen(process.env.LISTENERPORT);
 console.log('Faye listening on port ' + process.env.LISTENERPORT);
