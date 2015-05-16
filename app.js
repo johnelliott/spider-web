72498974aef156f2f6b903144075a7748d43fdef
John Elliott <johnelliott703@gmail.com>
2015-05-16 12:47:06 -0400

    move serial connection to a module have server listen on configured port

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /*jslint node: true */
-
 var http = require('http');
 var SocketServer = require('socket.io');
+var sp = require("./lib/serial");
 // env
 var config = require('./config');
 var port = process.env.PORT || 8000;
 var sockPort = process.env.SOCKPORT || 8001;
+var localSerialPort = process.env.LOCALSERIALPORT || '/dev/ttyACM0'; // default to Ras. Pi USB
 
 // http server
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello HTTP\n");
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end("Hello HTTP\n");
 });
 
 // Socket.io sockets server
@@ -18,37 +19,20 @@ var socketServer = new SocketServer(server);
 // socketServer(server);
 
 socketServer.on('connection', function(socket){
-  socket.on('hit', function(data){
-    console.log("socket hit event", data);
-  });
-  socket.on('disconnect', function(){
-    console.log("socket disconnect event");
-  });
+	socket.on('hit', function(data){
+		console.log("socket hit event", data);
+	});
+	socket.on('disconnect', function(){
+		console.log("socket disconnect event");
+	});
 });
 
-// modules
-// var counter = require("./lib/serial");
+// console.log("localSerialPort", localSerialPort);
+var counter = sp.connect(localSerialPort);
 
-// serial port
-var serialport = require("serialport");
-var localSerialPort = process.env.LOCALSERIALPORT || '/dev/ttyACM0'; // default to Ras. Pi USB
-
-// Create a serial port
-var sp = new serialport.SerialPort(localSerialPort, {
-    parser: serialport.parsers.readline("\n"),
-    baudrate: 9600
-});
-// Open serial connection
-sp.open(function (err) {
-    if(err) {
-        console.log('Failed to open serial port', err);
-    }
-    else {
-        console.log('Serial port open', sp.path);
-    }
+counter.on("data", function(data) {
+	console.log("serial port data", data);
+	socketServer.emit("hit", data);
 });
 
-sp.on("data", function(data) {
-    console.log("serial port data", data);
-    socketServer.emit("hit", data);
-});
+server.listen(port);
