c9ae1ae7a7ddbeb950a18a7882edaa398a22460e
John Elliott <johnelliott703@gmail.com>
2015-05-16 13:56:36 -0400

    add test suite

---
+++ test/index.js
@@ -0,0 +1,30 @@
+var request = require("request");
+var should = require("should");
+require("./index");
+
+describe("Serial", function() {
+    var serial = require("../lib/serial");
+    it("should return a connect function", function() {
+        serial.connect.should.be.type("function");
+    });
+});
+
+describe("Server", function() {
+    var app = require("../app.js");
+    it('should respond to http GET requests', function(done) {
+        request.get('http://localhost:8000/', function(err, res, body) {
+            if(err) {
+                throw new Error(err);
+            }
+            body.should.equal("Hello HTTP\n");
+            done();
+        });
+    });
+});
+
+describe.skip("Sockets server", function() {
+    var app = require("../app.js");
+    it('should respond to socket requests', function() {
+       // TODO
+    });
+});
